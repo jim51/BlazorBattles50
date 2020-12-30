3a2fe58999c76d5a64c31fc079080008eccbb784
JIm <kstsabc@gmail.com>
2020-12-30 15:56:41 +0800

    **新增服務,註冊服務,訂閱事件,Dispose事件,

---
+++ BlazorBattles50/Client/Services/BananaService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles50.Client.Services
+{
+    public class BananaService : IBananaService
+    {
+        public int Bananas { get; set; } = 1000;
+
+        public event Action OnChange;
+
+        public void EatBananas(int amouont)
+        {
+            Bananas -= amouont;
+            BananasChanged();
+        }
+
+        void BananasChanged() => OnChange.Invoke();
+    }
+}
