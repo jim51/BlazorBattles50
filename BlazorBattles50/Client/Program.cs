f5984de66a3b64d622dd7b3550c6fccaadca6362
JIm <kstsabc@gmail.com>
2021-01-03 15:36:09 +0800

    增加Blazored.Toast,並使用顯示建立失敗錯訊息

--- BlazorBattles50/Client/Program.cs
+++ BlazorBattles50/Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorBattles50.Client.Services;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +18,7 @@ namespace BlazorBattles50.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.Services.AddBlazoredToast();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IBananaService, BananaService>();
             builder.Services.AddScoped<IUnitService, UnitService>();
