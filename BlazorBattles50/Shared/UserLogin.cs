8281a7a39f342d3e19431ae358de9136c47ee136
JIm <kstsabc@gmail.com>
2021-01-19 11:31:46 +0800

    加入Login類別,EditForm Bind,DataAnntationsValidator

---
+++ BlazorBattles50/Shared/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles50.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage ="請輸入帳號")]
+        public string username { get; set; }
+        [Required]
+        public string password { get; set; }
+    }
+}
