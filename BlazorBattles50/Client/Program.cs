6b0edc5bdd6e919ebdfa4dccaf0a9ab8f81fe1cd
JIm <kstsabc@gmail.com>
2021-05-05 11:02:55 +0800

    驗證:加入Blazored.LocalStorage及參考

--- BlazorBattles50/Client/Program.cs
+++ BlazorBattles50/Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorBattles50.Client.Services;
+using Blazored.LocalStorage;
 using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -20,6 +21,7 @@ namespace BlazorBattles50.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.Services.AddBlazoredToast();
+            builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IBananaService, BananaService>();
             builder.Services.AddScoped<IUnitService, UnitService>();
