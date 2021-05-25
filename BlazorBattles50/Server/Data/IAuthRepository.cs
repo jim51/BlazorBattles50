a3459a4460260b33eed34cc1ab8b2b889a24d5cc
JIm <kstsabc@gmail.com>
2021-05-25 20:01:39 +0800

    AUTH:驗證準備(Interface,Repository),DI

---
+++ BlazorBattles50/Server/Data/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using BlazorBattles50.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles50.Server.Data
+{
+    public interface IAuthRepository
+    {
+        Task<int> Regiseter(User user, string password);
+        Task<string> Login(string email, string password);
+        Task<bool> UserExists(string email);
+    }
+}
