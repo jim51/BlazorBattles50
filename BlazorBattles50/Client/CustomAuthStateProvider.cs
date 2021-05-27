5a9853506b6c2ab0ae56029060dcae079bbaf09b
JIm <kstsabc@gmail.com>
2021-05-27 09:23:08 +0800

    JWT:UI:寫入Token,讀取Token,

--- BlazorBattles50/Client/CustomAuthStateProvider.cs
+++ BlazorBattles50/Client/CustomAuthStateProvider.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BlazorBattles50.Client
@@ -11,29 +14,64 @@ namespace BlazorBattles50.Client
     public class CustomAuthStateProvider : AuthenticationStateProvider
     {
         private readonly ILocalStorageService _localStorageService;
+        private readonly HttpClient _http;
 
-        public CustomAuthStateProvider(ILocalStorageService localStorageService)
+        public CustomAuthStateProvider(ILocalStorageService localStorageService, HttpClient http)
         {
             _localStorageService = localStorageService;
+            _http = http;
         }
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var state = new AuthenticationState(new System.Security.Claims.ClaimsPrincipal());
-            if (await _localStorageService.GetItemAsync<bool>("IsAuthenticated"))
+            string authToken =await _localStorageService.GetItemAsStringAsync("authToken");
+            var identity = new ClaimsIdentity();
+            _http.DefaultRequestHeaders.Authorization = null;
+            if (!string.IsNullOrWhiteSpace(authToken))
             {
-                var identity = new ClaimsIdentity(
-                new[]
-                {
-                        new Claim(ClaimTypes.Name,"Jim")
-                }, "test authentication type");
-                var user = new ClaimsPrincipal(identity);
-                state = new AuthenticationState(user);
-             
-                //return Task.FromResult(new AuthenticationState(user));
+                identity = new ClaimsIdentity(ParseClaimsFromJwt(authToken), "jwt");
+                _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
             }
+          
+
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
             NotifyAuthenticationStateChanged(Task.FromResult(state));
             return state;
+            //var state = new AuthenticationState(new System.Security.Claims.ClaimsPrincipal());
+            //if (await _localStorageService.GetItemAsync<bool>("IsAuthenticated"))
+            //{
+            //    var identity = new ClaimsIdentity(
+            //    new[]
+            //    {
+            //            new Claim(ClaimTypes.Name,"Jim")
+            //    }, "test authentication type");
+            //    var user = new ClaimsPrincipal(identity);
+            //    state = new AuthenticationState(user);
+             
+            //    //return Task.FromResult(new AuthenticationState(user));
+            //}
+            //NotifyAuthenticationStateChanged(Task.FromResult(state));
+            //return state;
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "==";break;
+                case 3: base64 += "=";break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+
+        public IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValueParis = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            var claims = keyValueParis.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+            return claims;
         }
     }
 }
