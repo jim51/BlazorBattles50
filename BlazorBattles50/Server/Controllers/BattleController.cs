95de8e4296073c29f6bc126d97269daa2c682dcc
JIm <kstsabc@gmail.com>
2021-05-30 00:24:55 +0800

    API:建立基本戰鬥Controller

---
+++ BlazorBattles50/Server/Controllers/BattleController.cs
@@ -0,0 +1,41 @@
+﻿using BlazorBattles50.Server.Data;
+using BlazorBattles50.Server.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles50.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BattleController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IUtilityService _utilityService;
+
+        public BattleController(DataContext context, IUtilityService utilityService)
+        {
+            this._context = context;
+            this._utilityService = utilityService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StartBattle([FromBody] int opponentId)
+        {
+            var attacker = await _utilityService.GetUser();
+            var opponent = await _context.Users.FindAsync(opponentId);
+            if(opponent==null || opponent.IsDeleted)
+            {
+                return NotFound("使手者不存在!");
+            }
+
+            return Ok();
+        }
+    }
+}
