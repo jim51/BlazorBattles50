35433725b42bfae0abf6e7ecc524890fe9e97c4d
JIm <kstsabc@gmail.com>
2021-05-29 20:01:18 +0800

    DB:增加User戰鬥狀態 DB

---
+++ BlazorBattles50/Server/Migrations/20210529115842_UserBattlesStatistics.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorBattles50.Server.Migrations
+{
+    public partial class UserBattlesStatistics : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Battles",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Defeats",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Victories",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Battles",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Defeats",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Victories",
+                table: "Users");
+        }
+    }
+}
--- BlazorBattles50/Shared/User.cs
+++ BlazorBattles50/Shared/User.cs
@@ -19,5 +19,8 @@ namespace BlazorBattles50.Shared
         public bool IsDeleted { get; set; }
         public DateTime DateCreate { get; set; } = DateTime.Now;
         public List<UserUnit> Units { get; set; }
+        public int Battles { get; set; }
+        public int Victories { get; set; }
+        public int Defeats { get; set; }
     }
 }
