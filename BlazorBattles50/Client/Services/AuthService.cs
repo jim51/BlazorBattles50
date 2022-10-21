aef642ad9e275eb2ae838613a62eb1cbee4c88c4
JIm <kstsabc@gmail.com>
2022-10-21 22:04:56 +0800

    Login

--- BlazorBattles50/Client/Services/AuthService.cs
+++ BlazorBattles50/Client/Services/AuthService.cs
@@ -1,4 +1,8 @@
 ﻿using BlazorBattles50.Shared;
+using Blazored.LocalStorage;
+using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,21 +15,48 @@ namespace BlazorBattles50.Client.Services
     public class AuthService : IAuthService
     {
         private readonly HttpClient _http;
+        private readonly ILocalStorageService _localStorageService;
+        private readonly AuthenticationStateProvider _authStateProvider;
+        private readonly IToastService _toastService;
+        private readonly NavigationManager _navigationManager;
 
-        public AuthService(HttpClient http)
+        public AuthService(HttpClient http, ILocalStorageService localStorageService
+            , AuthenticationStateProvider AuthStateProvider
+            , IToastService toastService
+            , NavigationManager navigationManager)
         {
             _http = http;
+            _localStorageService = localStorageService;
+            _authStateProvider = AuthStateProvider;
+            _toastService = toastService;
+            this._navigationManager = navigationManager;
         }
 
-        public async Task<ServiceResponse<string>> Login(UserLogin request)
+        public async Task Login(UserLogin request)
         {
             var result = await _http.PostAsJsonAsync("api/auth/login", request);
-            return await result.Content.ReadFromJsonAsync<ServiceResponse<string>>();
+            var response = await result.Content.ReadFromJsonAsync<ServiceResponse<string>>();
+            if (response.Success)
+            {
+                await _localStorageService.SetItemAsync<string>("authToken", response.Data);
+                await _authStateProvider.GetAuthenticationStateAsync();
+            }
+            else
+            {
+                _toastService.ShowError(response.Message);
+            }
+        }
+
+        public async Task Logout()
+        {
+            await _localStorageService.RemoveItemAsync("authToken");
+            await _authStateProvider.GetAuthenticationStateAsync();
+            _navigationManager.NavigateTo("/");
         }
 
         public async Task<ServiceResponse<int>> Register(UserRegister request)
         {
-            var result = await _http.PostAsJsonAsync("api/Auth/register",request);
+            var result = await _http.PostAsJsonAsync("api/Auth/register", request);
             return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
         }
     }
--- BlazorBattles50/Client/Services/IAuthService.cs
+++ BlazorBattles50/Client/Services/IAuthService.cs
@@ -9,6 +9,7 @@ namespace BlazorBattles50.Client.Services
     public interface IAuthService
     {
         Task<ServiceResponse<int>> Register(UserRegister request);
-        Task<ServiceResponse<string>> Login(UserLogin request);
+        Task Login(UserLogin request);
+        Task Logout();
     }
 }
